79c8f4f93a6a59a5c2d15df6896e0613f9ff219b
loi219 <sven@rouvinez.me>
2015-12-12 14:27:55 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+include($_SERVER["DOCUMENT_ROOT"]."/head_foot/header.php");
+include($_SERVER["DOCUMENT_ROOT"]."/head_foot/footer.php");
+?>
+
+
+<div class="text-center">
+    <h1>Bibliothèque partagée</h1>
+    <p class="lead">Bienvenue sur le projet de la bibliothèque partagée
+
+    </p>
+    <p class="lead">
+        Utilisez le menu ci-dessus pour afficher et ajouter les livres.
+
+    </p>
+    <p class="lead">
+        Et le menu déroulant à droite pour afficher vos livres et vous déconnectez.
+    </p>
+
+</div>
