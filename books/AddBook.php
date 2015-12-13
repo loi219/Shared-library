ff0978ecad9df9009ce7fa81de4a771c2efd48fa
loi219 <sven@rouvinez.me>
2015-12-13 19:29:49 +0100

    test

--- books/AddBook.php
+++ books/AddBook.php
@@ -1,17 +1,26 @@
 <?php
+include($_SERVER["DOCUMENT_ROOT"]."/head_foot/footer.php");
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 class AddBook{
 
 
 function __construct($arrayBooks){
 
+
 $this->showValuesBooks($arrayBooks);
+    $this->createQuery($_POST['id']);
 
 }
 
 
     function createQuery($arrayBookValue){
-        $bookValidation = new SearchBooks();
+
+       $query="INSERT INTO books(title) VALUES ('$arrayBookValue')";
+    $this->addBook($query);
+
+        /** $bookValidation $bookValidation = new SearchBooks();
 
         $date= time();
         $isbn=$arrayBookValue['isbn'];
@@ -28,17 +37,29 @@ $this->showValuesBooks($arrayBooks);
         } else {
             echo "<p>Livre $title déjà ajouté</p>";
 
-        }
+        } **/
 
     }
 
 
+    function addBook($query)
+    {
 
+        $db = new Database();
+
+
+        if ($db->error()) {
+            echo $db->error();
+
+        } else {
+echo $query;
+            $db->query($query);
+
+
+
+        }
+    }
 private function showValuesBooks($array){
-
-
-
-
 
 ?>
 
@@ -51,8 +72,35 @@ private function showValuesBooks($array){
         <option value="<?php echo "1" ?>"><?php echo $book['title']; }?></option>
 
     </select>
-        <?php }} ?>
-        <hr>
+        <button class="triggerClass" id="<?php echo $book['title']; ?>">Ajouter</button>
+
+        <?php
+
+
+        ?>
+
     </div>
 </div>
+<script>
 
+    $(document).ready(function(){
+        $(".triggerClass").click(function(){
+
+            var yourId = $(this).attr("id");
+            alert(yourId);
+
+            $.ajax({
+                url: "/books/AddBook.php",
+                method: "GET",
+                data: { id : yourId },
+                dataType: "html"
+            });
+
+
+        });
+    });
+
+
+</script>
+
+<?php } }?>
